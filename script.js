149f928ce33e22dd8457858c95d0dc56c3f47b91
senuracmps@gmail.com <senuracmps@gmail.com>
2026-06-01 14:30:50 +0530

    task

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function showMessage(){
     document.getElementById("message").innerHTML="CI/CD Pipeline Working!";
 }
+function showMessage2(){
+    document.getElementById("message").innerHTML="Senura Nimsara";
+}
